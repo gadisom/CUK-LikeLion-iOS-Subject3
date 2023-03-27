939f32ecc7d36669b70bcf3dcd3f740246ae111f
gadisom <jeongwon.kim64@gmail.com>
2023-03-28 01:45:50 +0900

    feat : Person, CoffeeShop, Coffee type 정의

--- homework3/homework3/main.swift
+++ homework3/homework3/main.swift
@@ -1,40 +1,57 @@
 import Foundation
 class Person {
+
     var money : Int
-    init (myMoney : Int)
+    var name : String
+
+    init (myMoney : Int , name : String)
     {
         self.money = myMoney
+        self.name = name
     }
-    
-    func buyThings (price : Int) -> Int
+
+    func buy (price : Int) -> Bool // price가 money보다 작으면 false
     {
-        return money - price
+        return price <= money
     }
-    
+
 }
+
 class CoffeeShop {
-    
-    var sales : Int = 0
+
+    var sales : Int
     var barista : Person?
-    var menu : [String : Int]?
-    var pickUpTable : [String] = []
-    
-    init(barista: Person) {
+    var menu : [Coffee : Int]? // 커피 종류: 가격
+    var pickUpTable : [Coffee] = []
+
+    init(barista: Person, sales: Int) {
         self.barista = barista
-    }
-    
-    func order (coffee : Coffee) {
+        self.sales = sales
         
     }
     
-    enum Coffee {
-        case americano ,latte ,tea ,capuccino
+    func order (coffee : Coffee ,person : Person)  {
+
+        guard let menuExist = menu?[coffee] else {
+            print("메뉴에 없는 것입니다.")
+            return
+            
+        }
+        pickUpTable.insert(coffee, at:pickUpTable.endIndex)
+        
     }
 }
 
-let misterLee = Person(myMoney: 1000)
-let missKim = Person(myMoney: 30000)
-let mutsabucks = CoffeeShop(barista: misterLee)
+enum Coffee
+{
+    case americano ,latte ,tea ,capuccino
+}
+
+
+let misterLee = Person(myMoney: 1000 ,name :"misterLee")
+let missKim = Person(myMoney: 2000,name :"missKim")
+let mutsabucks = CoffeeShop(barista: misterLee, sales: 0)
+
 
 
 
