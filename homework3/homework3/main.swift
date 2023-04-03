2efec63f4d05a0c2b595839c28c05856f62b8368
gadisom <jeongwon.kim64@gmail.com>
2023-04-03 18:13:51 +0900

    feat : 구매기능, 사용자 잔액 기능, 픽업테이블 기능 추가

--- homework3/homework3/main.swift
+++ homework3/homework3/main.swift
@@ -1,43 +1,97 @@
-import Foundation
 
+import Foundation
 struct Person {
-    
+
     var money : Int
     var name : String
-    
-    mutating func buy (itemPrice : Int) -> Int
+
+    // 구매가 가능한지 여부
+    mutating func buy (itemPrice : Int) -> Bool
     {
-        return money - itemPrice
+        if money >= itemPrice
+        {
+            money -= itemPrice
+            print("""
+                    *입출금 내역*
+                    출금 : \(itemPrice)
+                    \(name)님의 잔액은 \(money)원 입니다.
+                    """)
+            return true
+        }
+        
+        else {
+            print("잔액이 \(itemPrice - money)원 부족합니다.\n")
+            return false
+        }
     }
-    
+
 }
 
 struct CoffeeShop {
-    
+
     var sales : Int
     var barista : Person?
     var menu : [Coffee : Int]?
     var pickUpTable : [Coffee] = []
-    
-    mutating func order (coffee : Coffee ,person : Person)  {
+
+    mutating func order (coffee : Coffee ,customer : inout Person)  {
         
-        guard let menuExist = menu?[coffee] else {
-            print("메뉴에 없는 것입니다.")
+        guard barista != nil else {
+            print("바리스타가 없습니다. \n")
             return
-            
         }
+        
+        guard let itemPrice = menu?[coffee] else {
+            print("메뉴에 없는 것입니다.\n")
+            return
+        }
+        
+        guard customer.buy(itemPrice: itemPrice) else {
+            return
+        }
+        
+        sales += itemPrice
         pickUpTable.insert(coffee, at:pickUpTable.endIndex)
         
+        // 메뉴를 제공
+        provideItem(coffee: coffee, customer: &customer)
+        
     }
-    
+    mutating func provideItem (coffee: Coffee, customer : inout Person)
+    {
+        let orderNumber = pickUpTable.count
+        let customer_name = customer.name
+        let ordered_menu = pickUpTable[pickUpTable.endIndex-1]
+        
+        print("""
+            
+            대기번호 :\(orderNumber)
+            \(customer_name)님의
+            \(ordered_menu) 준비되었습니다.
+            픽업대에서 가져가주세요
+            
+            """)
+    }
+
 }
 
 enum Coffee {
+    
     case americano ,latte ,tea ,capuccino
 }
 
 
-let misterLee = Person(money: 1000 ,name :"misterLee")
-let missKim = Person(money: 2000,name :"missKim")
-let mutsabucks = CoffeeShop(sales: 0, barista: misterLee)
+var misterLee = Person(money: 10000 ,name :"misterLee")
+var missKim = Person(money: 6000,name :"missKim")
+var mutsabucks = CoffeeShop(sales: 0, barista: misterLee)
+var starbucks = CoffeeShop(sales: 0)
+
+mutsabucks.menu = [.americano:2000,.latte:3000,.tea:4000,.capuccino:3500]
+starbucks.menu = [.americano:2000]
+
+mutsabucks.order(coffee: .americano, customer: &missKim)
+mutsabucks.order(coffee: .latte, customer: &missKim)
+mutsabucks.order(coffee: .tea, customer: &missKim)
+starbucks.order(coffee: .americano, customer: &misterLee)
+
 
--- subject3.md
+++ subject3.md
@@ -130,3 +130,74 @@ print(clsBB.a) // 20
 깊은복사 : 
 
 얕은복사 : 값만 복사를 하는 것
+
+---
+
+#### inout 이란 ? 
+
+기본적으로 함수 내부에서 파라미터로 전달된 인스턴스는 상수타입이다. 
+따라서 customer는 상수 타입이고 buy 메소드는 mutating 로 되어있다. 
+buy는 mutating 메소드 이기 때문에 상수를 변경할 수 없다. 이를 해결하기 위해서 inout 키워드 사용 
+
+inout 은 함수 내부에서 매개변수의 값을 변경하고 이를 함수 외부에서도 유지할 때 사용. 즉 , 함수 호출 시 매개변수에 전달되는 값을 
+함수 내부에서 직접 수정할 수 있도록 한다. 
+
+``` swift 
+
+import Foundation
+struct Person {
+
+    var money : Int
+    var name : String
+
+    mutating func buy (price : Int) // 구매했을때 돈을 차감시켜줌
+    {
+        money = money - price
+    }
+
+}
+
+struct CoffeeShop {
+
+    var sales : Int
+    var barista : Person?
+    var menu : [Coffee : Int]?
+    var pickUpTable : [Coffee] = []
+    
+    mutating func order (coffee : Coffee ,customer : Person)  {
+
+            customer.buy(price: coffeePrice) // 가능할까 ?? 
+    }
+
+}
+
+```
+
+---
+
+inout , class = 모두 참조타입 
+ 
+1. inout : 값을 복사하지 않고 인자로 전달된 변수의 메모리 주소를 참조하여 해당 변수의 값을 변경 
+    - 값 타입인 구조체나 열거형 타입에서만 사용 가능 
+
+2. class : 변수나 상수가 인스턴스를 참조하기 때문에 함수 내에서 인스턴스의 내용을 수정할 수 있다. 
+
+-> 즉 값 타입에서는 Inout 키워드를 사용해야 하지만, 참조타입에서는 그냥 인스턴스를 전달하면 된다. 
+
+
+``` swift 
+var a = 10
+var b = 20
+
+func swap(_ a: inout Int, _ b: inout Int) {
+    let temp = a
+    a = b
+    b = temp
+}
+
+swap(&a, &b) // & 기호는 해당 변수의 메모리 주소를 전달하기 위한 기호이다. 인자로 전달할 때 & 기호를 붙여줘야 한다. 
+
+```
+
+--- 
+
